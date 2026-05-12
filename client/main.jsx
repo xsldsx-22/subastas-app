49b2e8e7cd4558bed51bc50f346cd15ea2d6b602
Karen Guzman <User@LAPTOP-82B6DRRS.local.itiztapalapa>
2026-05-12 16:57:52 -0600

    Fix remaining auctions import

--- client/main.jsx
+++ client/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Meteor } from "meteor/meteor";
-import "./Main.css";
+import "./main.css";
 import { MainLayout } from "../imports/ui/layouts/MainLayout";
 import { Toaster } from "react-hot-toast";
 
